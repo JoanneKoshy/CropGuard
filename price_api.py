e084fd3d514bf3e70f14f1ff0aed907ab64fc425
Joanne Alice Thomas <161133806+JoanneKoshy@users.noreply.github.com>
2025-02-02 23:34:53 +0530

    Update price_api.py

--- price_api.py
+++ price_api.py
@@ -1,6 +1,6 @@
 import requests
 
-AGMARKNET_API_KEY = "579b464db66ec23bdd000001cdd3946e44ce4aad7209ff7b23ac571b"
+AGMARKNET_API_KEY = "" #place the api key over here 
 
 # Manually defined crop prices (in ₹ per quintal)
 manual_prices = {
